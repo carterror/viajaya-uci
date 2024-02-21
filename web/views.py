0d3a221390c1defe86b79c0e22f459482faa79f8
Mr-3b0y <mr_3b0y@protonmail.com>
2024-02-21 02:36:19 -0500

    add cruds and more

--- web/views.py
+++ web/views.py
@@ -7,8 +7,18 @@ from django.db.models.functions import TruncDate
 
 # Create your views here.
 def home(request):
+    transport_curiosities = [
+        {'curiosity': 'El primer avión de pasajeros voló en  1908.', 'source': 'Historia del Avión'},
+        {'curiosity': 'El primer automóvil de producción en masa fue el Ford Model T, lanzado en  1908.', 'source': 'Historia del Automóvil'},
+        {'curiosity': 'El primer tren de alta velocidad, el Shinkansen, comenzó a operar en Japón en  1964.', 'source': 'Historia del Tren'},
+        {'curiosity': 'El primer sistema de metro subterráneo fue inaugurado en Londres en  1863.', 'source': 'Historia del Metro'},
+        {'curiosity': 'El primer ferry de hidrógeno, el "Hyperion", comenzó a operar en  2021.', 'source': 'Innovaciones en Transporte'},
+    ]
     rutas = Ruta.objects.all()
-    return render(request, 'web/home.html', {'rutas': rutas})
+    
+    context = {'rutas': rutas, 'transport_curiosities': transport_curiosities}
+    
+    return render(request, 'web/home.html', context)
 
 def buscar(request):
     if request.method == 'POST':
